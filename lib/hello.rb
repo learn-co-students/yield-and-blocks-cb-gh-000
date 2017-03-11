ef4183f739d770e1a9e70dec2b42c6be0fc4adfe
LuisRodriguez <CodeKommissar@gmail.com>
2017-03-11 15:46:05 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
-
+def hello_t(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    ret = yield array[i]
+    if ret != nil
+      new_array.push ret
+    end
+    i = i + 1
+  end
+  new_array
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.upcase.start_with?("T")
+        puts "Hi, #{name}"
+        name
+    end
+end
--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,17 +1,22 @@
 require "spec_helper"
 
-describe "#hello_t" do 
+describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
-  it "only passes names that start with 'T' to the block" do 
-    expect{hello_t(names){|name| puts "Hi, #{name}" }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
+  it "only passes names that start with 'T' to the block" do
+    expect{hello_t(names){|name| puts "Hi, #{name}" if name.upcase.start_with?("T") }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
   end
 
   it "returns an array with only names that start with 'T'" do
-    expect(hello_t(names) {|name| puts "Hi, #{name}" }).to eq(["Tim", "Tom"])
+    expect(hello_t(names) do |name|
+        if name.upcase.start_with?("T")
+            puts "Hi, #{name}"
+            name
+        end
+    end).to eq(["Tim", "Tom"])
   end
 
   it "is case insensitive" do
     other_names = ["tim", "tom", "jim"]
-    expect{hello_t(other_names){|name| puts "Hi, #{name}" }}.to output("Hi, tim\nHi, tom\n").to_stdout
+    expect{hello_t(other_names){|name| puts "Hi, #{name}" if name.upcase.start_with?("T") }}.to output("Hi, tim\nHi, tom\n").to_stdout
   end
 end
