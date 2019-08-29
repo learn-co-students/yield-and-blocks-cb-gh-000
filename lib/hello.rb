3b5e49b097f3340ee071de3b4aa185bd0e7a0176
sonny miel <sonnymt@hotmail.com>
2019-08-29 09:43:39 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
-
+def hello_t(arg)
+  if block_given?
+    arg.each do |a|
+      yield(a)
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
